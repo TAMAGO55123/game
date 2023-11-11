2fb881974db8ddafbf6d66573d47271e61fe856a
TAMAGO55123 <147392915+TAMAGO55123@users.noreply.github.com>
2023-11-11 14:59:55 +0900

    update

--- game/js/engine/addbutton.js
+++ game/js/engine/addbutton.js
@@ -17,31 +17,27 @@ button.attack=document.createElement('button');
 button.attack.id='attack';
 button.attack.classList.add('tn-custom01-front');
 button.attack.innerHTML='戦う';
-buttonfunction.attack=function(){};
-button.attack.onclick=buttonfunction.attack;
+
 
 button.magic=document.createElement('button');
 button.magic.id='magic';
 button.magic.classList.add('tn-custom01-front');
 button.magic.innerHTML='魔法';
-buttonfunction.magic=function(){};
-button.magic.onclick=buttonfunction.magic;
+
 
 button.guard=document.createElement('button');
 button.guard.id='guard';
 button.guard.classList.add('tn-custom01-front');
 button.guard.innerHTML='防御';
-buttonfunction.guard=function(){};
-button.guard.onclick=buttonfunction.guard;
+
 
 button.mydata=document.createElement('button');
 button.mydata.id='mydata';
 button.mydata.classList.add('tn-custom01-front');
 button.mydata.innerHTML='スキル';
-buttonfunction.mydata=function(){};
-button.mydata.onclick=buttonfunction.mydata;
 
-var br=document.createElement('sb');
+
+var br1=document.createElement('sb');
 var brscriptdata=document.createElement('script');
 brscriptdata.src='/lib/br.js';
 
@@ -49,7 +45,7 @@ document.querySelector('div#area').appendChild(stagedata.stage);
 document.querySelector('div#area').appendChild(stagedata.textarea);
 document.querySelector('div#button').appendChild(button.attack);
 document.querySelector('div#button').appendChild(button.magic);
-document.querySelector('div#button').appendChild(br);
+document.querySelector('div#button').appendChild(br1);
 document.querySelector('div#button').appendChild(button.guard);
 document.querySelector('div#button').appendChild(button.mydata);
 document.querySelector('div#button').appendChild(brscriptdata);
