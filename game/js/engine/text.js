ac1c53a5e2b4a52bf1cf37b277b45583511fd630
TAMAGO55123 <147392915+TAMAGO55123@users.noreply.github.com>
2023-11-11 12:46:31 +0900

    update

--- game/js/engine/text.js
+++ game/js/engine/text.js
@@ -6,7 +6,13 @@
  * @param {TextSizepx} textpx 
  */
 function textdot(text,textarea,time,textpx){
-    let data2=data.split('');
-    let data3=window.innerWidth/textpx;
-    
+    let data2=text.split('');
+    let data3=(window.innerWidth-(textpx*2))/textpx;
+    let data4='';
+    let data5=data2.length;
+    for (let a = 0; a < data5; a++) {
+        data4 = data4+data5[a];
+        textarea.innerHTML=data4;
+        wait(time);
+    }
 }
